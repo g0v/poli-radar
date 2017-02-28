5d018d2620ecd9423233a456d25fd49b6fffcca9
Pomin Wu <pomin5@gmail.com>
2017-02-28 09:07:45 -0500

    Changed production backend API endpoint

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const forceRemote = false;
 
-const remoteUri = 'https://mysterious-citadel-44315.herokuapp.com/api';
+const remoteUri = 'https://poli-radar-api.herokuapp.com/api';
 const localUri = 'http://localhost:8000/api';
 
 export const BASE_URI = forceRemote ? remoteUri : ( // eslint-disable-line no-nested-ternary
